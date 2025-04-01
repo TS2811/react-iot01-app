844c8919430c9b335c2ca625a9016472f670bd0b
TS2811 <S6419410003@sau.ac.th>
2025-04-01 11:04:36 +0700

    Code: V2

--- src/views/HomeMenu.jsx
+++ src/views/HomeMenu.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Box, Typography, Button, Avatar } from '@mui/material'
 
 export default function HomeMenu() {
   return (
     <>
-      TS03
+      <Box sx={{ height: '100vh', display: 'flex' }}>
+        <Box sx={{ width: '70%', boxShadow: 2, mx: 'auto', my: 'auto', p: 3 }}>
+          <Avatar src={'https://cdn.pixabay.com/photo/2023/03/13/15/21/iot-7850195_1280.jpg'} sx={{ width: 150, height: 150, mx: 'auto' }}></Avatar>
+          <Typography variant='h3' sx={{ textAlign: 'center', mt: 2 }}>IoT Calculate by Thanawat SAU</Typography>
+        </Box>
+      </Box>
     </>
   )
 }
+
